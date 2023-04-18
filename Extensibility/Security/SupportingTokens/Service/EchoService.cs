83b649d19989bdd15d4c17714039db1a8572ad31
Abdelwahab Afifi <v-aafifi@microsoft.com>
2023-04-18 10:12:29 +0200

    Add Security Supporting Tokens Sample

---
+++ Extensibility/Security/SupportingTokens/Service/EchoService.cs
@@ -0,0 +1,75 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Security.Cryptography.X509Certificates;
+using CoreWCF.IdentityModel.Claims;
+
+namespace CoreWcf.Samples.SupportingTokens
+{
+    // Service class which implements the service contract interface.
+    // Added code to write output to the console window
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
+    public class EchoService : IEchoService
+    {
+        public string Echo()
+        {
+            string userName;
+            string certificateSubjectName;
+            GetCallerIdentities(OperationContext.Current.ServiceSecurityContext, out userName, out certificateSubjectName);
+            return string.Format("Hello {0}, {1}", userName, certificateSubjectName);
+        }
+
+        public void Dispose()
+        {
+        }
+
+        bool TryGetClaimValue<TClaimResource>(ClaimSet claimSet, string claimType, out TClaimResource resourceValue)
+            where TClaimResource : class
+        {
+            resourceValue = default(TClaimResource);
+            IEnumerable<Claim> matchingClaims = claimSet.FindClaims(claimType, Rights.PossessProperty);
+            if (matchingClaims == null)
+                return false;
+            IEnumerator<Claim> enumerator = matchingClaims.GetEnumerator();
+            if (enumerator.MoveNext())
+            {
+                resourceValue = (enumerator.Current.Resource == null) ? null : (enumerator.Current.Resource as TClaimResource);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Returns the username and certificate subject name provided by the client
+        void GetCallerIdentities(ServiceSecurityContext callerSecurityContext, out string userName, out string certificateSubjectName)
+        {
+            userName = null;
+            certificateSubjectName = null;
+
+            // Look in all the claimsets in the authorization context
+            foreach (ClaimSet claimSet in callerSecurityContext.AuthorizationContext.ClaimSets)
+            {
+                if (claimSet is WindowsClaimSet)
+                {
+                    // Try to find a Name claim. This will have been generated from the windows username.
+                    string tmpName;
+                    if (TryGetClaimValue<string>(claimSet, ClaimTypes.Name, out tmpName))
+                    {
+                        userName = tmpName;
+                    }
+                }
+                else if (claimSet is X509CertificateClaimSet)
+                {
+                    // Try to find an X500DisinguishedName claim. This will have been generated from the client certificate.
+                    X500DistinguishedName tmpDistinguishedName;
+                    if (TryGetClaimValue(claimSet, ClaimTypes.X500DistinguishedName, out tmpDistinguishedName))
+                    {
+                        certificateSubjectName = tmpDistinguishedName.Name;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Extensibility/Security/SupportingTokens/Service/Program.cs
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+var builder = WebApplication.CreateBuilder();
+
+//Enable CoreWCF Services, with metadata (WSDL) support
+builder.Services.AddServiceModelServices()
+    .AddServiceModelMetadata()
+    .AddSingleton<IServiceBehavior, UseRequestHeadersForMetadataAddressBehavior>();
+
+var app = builder.Build();
+
+app.UseServiceModel(builder =>
+{
+    // Add the Calculator Service
+    builder.AddService<EchoService>(serviceOptions => { })
+    // Create the custom binding and add an endpoint to the service.
+    .AddServiceEndpoint<EchoService, IEchoService>(BindingHelper.CreateMultiFactorAuthenticationBinding(), "EchoService");
+    //Binding multipleTokensBinding = BindingHelper.CreateMultiFactorAuthenticationBinding();
+    //this.AddServiceEndpoint(typeof(IEchoService), multipleTokensBinding, string.Empty);
+
+    Action<ServiceHostBase> serviceHost = host => ChangeHostBehavior(host);
+    builder.ConfigureServiceHostBase<EchoService>(serviceHost);
+
+    // Configure WSDL to be available
+    var serviceMetadataBehavior = app.Services.GetRequiredService<ServiceMetadataBehavior>();
+    serviceMetadataBehavior.HttpGetEnabled = true;
+});
+
+app.Run();
+
+void ChangeHostBehavior(ServiceHostBase host)
+{
+    var srvCredentials = new ServiceCredentials();
+    srvCredentials.ServiceCertificate.SetCertificate("CN=localhost");
+    //srvCredentials.ServiceCertificate.SetCertificate(StoreLocation.LocalMachine, StoreName.My, X509FindType.FindBySubjectName, "localhost");
+    srvCredentials.ClientCertificate.Authentication.CertificateValidationMode = X509CertificateValidationMode.PeerOrChainTrust;
+    host.Description.Behaviors.Add(srvCredentials);
+}
